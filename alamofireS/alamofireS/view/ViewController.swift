3f5bd278b88981fa654633b966a7728363331c8f
ömer faruk coşar <ofcosar1@gmail.com>
2022-08-29 09:20:21 +0300

    view designed

--- alamofireS/alamofireS/view/ViewController.swift
+++ alamofireS/alamofireS/view/ViewController.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
-
+import Lottie
 class ViewController: UIViewController {
 
+    @IBOutlet var infoLabel: UILabel!
+    @IBOutlet var lottieAnimation: AnimationView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getInfoAction(_ sender: UIButton) {
+    }
+    
 }
 
