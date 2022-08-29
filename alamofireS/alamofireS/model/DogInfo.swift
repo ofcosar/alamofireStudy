c901b334c584fee78241ab4527fcd912888f788c
ömer faruk coşar <ofcosar1@gmail.com>
2022-08-29 09:22:38 +0300

    dogInfo class added

---
+++ alamofireS/alamofireS/model/DogInfo.swift
@@ -0,0 +1,14 @@
+//
+//  DogInfo.swift
+//  alamofireS
+//
+//  Created by ömer faruk coşar on 29.08.2022.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct DogInfo: Codable {
+    let facts: [String]
+    let success: Bool
+}
